28218f8486e317800a303850cb27d604937f5294
Ademola Ademeso <desmondademeso@gmail.com>
2024-07-06 00:27:33 +0100

    Added 1-hbtn_header.py

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -6,7 +6,7 @@ import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    req = urllib.request.Request(url)
 
+    req = urllib.request.Request(url)
     with urllib.request.urlopen(req) as response:
-        print(response.headers['X-Request-Id'])
+        print(dict(response.headers).get("X-Request-Id"))
