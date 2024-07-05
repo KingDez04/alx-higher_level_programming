25e38808df8a99cdac29b9477ddffa9677bbc690
Ademola Ademeso <desmondademeso@gmail.com>
2024-07-06 00:20:15 +0100

    Added 1-hbtn_header.py

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Displays the value of the X-Request-Id variable found in the header."""
+
+import urllib.request
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    req = urllib.request.Request(url)
+
+    with urllib.request.urlopen(req) as response:
+        print(response.headers['X-Request-Id'])
